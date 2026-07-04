81dd2dfef2f7acf130f19d9d2bb68fd491281ee2
MayasGhantous <102159871+MayasGhantous@users.noreply.github.com>
2026-07-05 00:19:46 +0300

    visualizer

--- gaussian_noise_analysis/vgg16/train.py
+++ gaussian_noise_analysis/vgg16/train.py
@@ -62,9 +62,9 @@ def main(prob, group_norm,Unet):
     criterion = nn.CrossEntropyLoss()
     
     if config.UNet:
-        model_visualizer = VGG16FeatureVisualizer(model.get_base_model(), unet=model.get_unet())
+        model_visualizer = CustomCNNFeatureVisualizer(model.get_base_model(), unet=model.get_unet())
     else:
-        model_visualizer = VGG16FeatureVisualizer(model)
+        model_visualizer = CustomCNNFeatureVisualizer(model)
 
     # 6. Train and finish
     train_model(model, train_loader, val_loader, val_loader2, val_loader3, criterion, optimizer, device,prog_vis =model_visualizer, config=config)
