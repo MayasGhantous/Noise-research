a2b46dec3e85c588c6626c05bbe51579a71ab552
MayasGhantous <102159871+MayasGhantous@users.noreply.github.com>
2026-07-05 01:02:09 +0300

    pushh

--- gaussian_noise_analysis/vgg16/network.py
+++ gaussian_noise_analysis/vgg16/network.py
@@ -11,36 +11,30 @@ from archtechre_common import *
 from Unet import UNetWrapper
 
 class CNN(nn.Module):
-    def __init__(self, begin_features=32, num_classes=1000):
+    def __init__(self, num_classes=1000):
         super(CNN, self).__init__()
         
-        # 1. OPTIMIZATION: Added stride=2 here! 
-        # This immediately shrinks the 224x224 image down to 112x112.
-        self.conv1 = nn.Conv2d(3, begin_features, kernel_size=3, stride=2, padding=1)
-        self.norm1 = nn.BatchNorm2d(begin_features)
-        
-        # Now conv2 only has to process a 112x112 grid, saving ~11 Billion operations
-        self.conv2 = nn.Conv2d(begin_features, begin_features * 2, kernel_size=3, padding=1)
-        self.norm2 = nn.BatchNorm2d(begin_features * 2)  
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2) # Shrinks to 56x56
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
+        self.norm1 = nn.GroupNorm(8, 32)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.norm2 = nn.GroupNorm(8, 64)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2) 
 
-        # conv3 and conv4 now process a 56x56 grid instead of 112x112
-        self.conv3 = nn.Conv2d(begin_features * 2, begin_features * 4, kernel_size=3, padding=1)
-        self.norm3 = nn.BatchNorm2d(begin_features * 4)  
-        self.conv4 = nn.Conv2d(begin_features * 4, begin_features * 4, kernel_size=3, padding=1)
-        self.norm4 = nn.BatchNorm2d(begin_features * 4)  
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2) # Shrinks to 28x28
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.norm3 = nn.GroupNorm(16, 128)
+        self.conv4 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+        self.norm4 = nn.GroupNorm(16, 128)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2) 
 
-        # conv5 and conv6 now process a 28x28 grid instead of 56x56
-        self.conv5 = nn.Conv2d(begin_features * 4, begin_features * 8, kernel_size=3, padding=1)
-        self.norm5 = nn.BatchNorm2d(begin_features * 8)  
-        self.conv6 = nn.Conv2d(begin_features * 8, begin_features * 8, kernel_size=3, padding=1)
-        self.norm6 = nn.BatchNorm2d(begin_features * 8)  
-        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2) # Shrinks to 14x14
+        self.conv5 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.norm5 = nn.GroupNorm(32, 256)
+        self.conv6 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.norm6 = nn.GroupNorm(32, 256)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2) 
 
         self.gap = nn.AdaptiveAvgPool2d((1, 1))
 
-        self.fc1 = nn.Linear(begin_features * 8, 128)
+        self.fc1 = nn.Linear(256, 128)
         self.norm_fc = nn.LayerNorm(128)
         self.drop = nn.Dropout(0.5) 
         self.fc2 = nn.Linear(128, num_classes)
@@ -69,6 +63,7 @@ class CNN(nn.Module):
         return x
 
     def initialize_weights(self):
+        
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
@@ -78,10 +73,10 @@ class CNN(nn.Module):
                 nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
                 if m.bias is not None:
                     nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.GroupNorm) or isinstance(m, nn.LayerNorm) or isinstance(m, nn.BatchNorm2d):
+            elif isinstance(m, nn.GroupNorm) or isinstance(m, nn.LayerNorm):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
-                
+
 if __name__ == "__main__":
     model = UNetWrapper(CNN(begin_features=128, num_classes=1000))
     #model size
